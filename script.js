8f1d1c4192a5b829c6431ff23b364920a8614e7a
Sivalilisha <sivalilishaadaikkalam@gmail.com>
2024-06-19 09:03:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function show(){
+    const el=document.getElementById("congrats");
+    el.innerHTML="Congrats! now you are also one of the member of our channel";
+}
+function clearFrom(){
+    document.getElementById("submit").reset();
+}
